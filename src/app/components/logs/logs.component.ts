41bbbe0a4e12346f9b6d3868e6b87909ab0418bb
evanpowell <evanpowellmusic@gmail.com>
2018-11-05 10:52:33 -0600

    add log service

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Log } from '../../models/Log';
+
+import { LogService } from '../../services/log.service';
+
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss']
 })
 export class LogsComponent implements OnInit {
+  logs: Log[];
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    this.logService.getLogs()
+      .subscribe(logs => this.logs = logs);
   }
 
+  onSelect(log: Log): void {
+    this.logService.setFormLog(log);
+  }
 }
---
+++ src/app/services/log.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+import { Log } from '../models/Log';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({ id: null, text: null, date: null});
+
+  selectedLog = this.logSource.asObservable();
+
+  constructor() {
+    this.logs = [
+      {
+        id: '1',
+        text: 'Generated components',
+        date: new Date('12/27/2017 12:54:23')
+      },
+      {
+        id: '2',
+        text: 'Added Bootstrap',
+        date: new Date('12/28/2017 11:20:23')
+      },
+      {
+        id: '3',
+        text: 'Added logs component',
+        date: new Date('12/27/2017 16:16:09')
+      },
+    ]
+  }
+
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+}
